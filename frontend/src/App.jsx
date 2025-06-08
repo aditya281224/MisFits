301558ab61caa39013bbf98bc39e7b872943503a
Aditya Sharma <adityasharmavats2811@gmail.com>
2025-06-08 12:07:52 +0530

    collectionPage with filters and sorting features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
 import {Routes,Route} from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
@@ -27,6 +28,8 @@ const App = () => {
         <Route path='/orders' element={<Orders></Orders>}/>
 
       </Routes>
+
+      <Footer/>
       
     </div>
   )
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+      <div>
+       Footer
+      </div>
+      
+    </div>
+  )
+}
+
+export default Footer
