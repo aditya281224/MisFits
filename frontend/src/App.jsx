d3c48bb155ad1cd45627beed2734e17fa0099a0a
Aditya Sharma <adityasharmavats2811@gmail.com>
2025-06-08 12:57:11 +0530

    SearchBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,12 +10,14 @@ import Orders from './pages/Orders'
 import {Routes,Route} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import SearchBar from './components/SearchBar'
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
 
 
       <Navbar/>
+      <SearchBar/>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/collection' element={<Collection></Collection>}/>
