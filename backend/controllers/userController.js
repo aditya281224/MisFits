80588261757dbc867ce9af07ac3c401e7bc58340
Aditya Sharma <adityasharmavats2811@gmail.com>
2025-06-16 16:36:01 +0530

    login and register logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,23 +1,77 @@
+import userModel from "../models/userModel.js";
+import validator from "validator";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
-//route for user login
-const loginUser = async (req,res) => {
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
 
-}
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
 
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "User does not exist",
+      });
+    }
 
-// route for user registration
+    const isMatch = await bcrypt.compare(password, user.password);
 
-const registerUser = async(req,res) =>{
+    if (isMatch) {
+      const token = createToken(user._id);
+      res.json({ success: true, token });
+    } else {
+      res.json({
+        success: false,
+        message: "Invalid Credentials",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
-  res.json({msg:"Register api working"})
-}
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const exits = await userModel.findOne({ email });
 
+    if (exits) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+    if (!validator.isEmail(email)) {
+      return res.json({ success: false, message: "Enter a valid email" });
+    }
 
-// admin login route
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Please enter a strong password",
+      });
+    }
 
-const adminLogin = async(req,res)=>{
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-}
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    const user = await newUser.save();
+    const token = createToken(user._id);
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
+const adminLogin = async (req, res) => {};
 
-export {loginUser,registerUser,adminLogin}
+export { loginUser, registerUser, adminLogin };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema({
   },{minimize:false})
 
 
-const userModel=mongoose.models.user ||  mongoose.model("user",userSchema);
+const userModel=mongoose.models.User ||  mongoose.model("User",userSchema);
 
 
-export default userModel
+export default userModel;
