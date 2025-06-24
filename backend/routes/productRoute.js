33d6c117906a5ca49b7d4a0516a242a8b796e0da
Aditya Sharma <adityasharmavats2811@gmail.com>
2025-06-24 15:06:25 +0530

    bug fixed

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -10,8 +10,8 @@ const productRouter = express.Router();
 productRouter.post('/add',adminAuth,upload.fields([{name:'image1',maxCount:1},{name:'image2',maxCount:1},{name:'image3',maxCount:1},{name:'image4',maxCount:1}]),addProduct);
 
 
-productRouter.post('/remove',adminAuth,removeProduct);
-productRouter.get('/list',adminAuth,listProduct);
-productRouter.post('/single',adminAuth,singleProduct); 
+productRouter.post('/remove',removeProduct);
+productRouter.get('/list',listProduct);
+productRouter.post('/single',singleProduct); 
 
 export default productRouter;
