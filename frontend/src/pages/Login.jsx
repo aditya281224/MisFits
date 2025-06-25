c617a9218cd0b5a3ef1821ef1083b6edb0051f26
Aditya Sharma <adityasharmavats2811@gmail.com>
2025-06-26 01:46:17 +0530

    changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -56,7 +56,7 @@ const Login = () => {
   },[token])
   return (
     
-      <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800 bg-gradient-to-r from-blue-100 to-yellow-50'>
+      <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800 '>
         <div className='inline-flex items-center gap-2 mb-2 mt-10'>
           <p className='prata-regular text-3xl'>{currentState}</p>
           <hr className='border-none h-[1.5px] w-8 bg-gray-800'/>
